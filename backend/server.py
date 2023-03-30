6f9bdf163c0f844c78e6c17538fc58fb86d181f8
yassineboujrada <yassine.boujrada@gmail.com>
2023-03-30 02:13:13 +0000

    add obj file

---
+++ backend/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['objFile']
+    print(file)
+    return {'message': 'File uploaded successfully'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
